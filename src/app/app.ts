d27f28eaf10096d09074913660d8fe166ed08e5a
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-09-30 21:53:10 +0330

    Installed many packages
    - Created index.ts
    - Created app.ts

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import type { Application } from "express";
+import morgan from "morgan";
+
+export function createApp(): Application {
+  const app: Application = express();
+
+  app.use(morgan(":method :url :status - :response-time ms"));
+
+  return app;
+}
