f5adc6e10e8acedc2a833fdfde8c906cb58ac5c8
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-10-01 12:02:11 +0330

    Created response.ts and types.ts

---
+++ src/helpers/response.ts
@@ -0,0 +1,57 @@
+import type { Request, Response } from "express";
+import type { ResponseObj } from "./types";
+
+export function makeResponseObj(
+  success: boolean,
+  message: string,
+  data: object = {}
+): ResponseObj {
+  return { success, message, data };
+}
+
+export function send404Error(req: Request, res: Response): Response {
+  const resObj = makeResponseObj(false, "Not found");
+
+  return res.status(404).json(resObj);
+}
+
+export function send405Error(allowedMethods: string[]): Function {
+  let allowHeaderValue = "";
+
+  for (const allowedMethod of allowedMethods) {
+    allowHeaderValue = `${allowHeaderValue}${allowedMethod}, `;
+  }
+  allowHeaderValue = allowHeaderValue.slice(0, -2);
+
+  return (req: Request, res: Response): Response => {
+    const resObj = makeResponseObj(
+      false,
+      `You can not ${req.method} ${req.baseUrl + req.path}`
+    );
+
+    res.set("Allow", allowHeaderValue);
+    return res.status(405).json(resObj);
+  };
+}
+
+export function generalErrorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: Function
+): Response {
+  console.log(`An error occurred: ${err.message}`);
+  console.log(err.stack);
+
+  const resObj = makeResponseObj(
+    false,
+    "Something went wrong while completing your request"
+  );
+
+  return res.status(500).json(resObj);
+}
+
+export const limitResponse = makeResponseObj(
+  false,
+  "You have reached your request limit"
+);
---
+++ src/helpers/types.ts
@@ -0,0 +1,5 @@
+export interface ResponseObj {
+  success: boolean;
+  message: string;
+  data: object;
+}
