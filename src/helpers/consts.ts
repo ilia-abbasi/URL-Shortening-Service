054ff6141ddd5358e1148d74a4592d38f713775c
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-10-01 17:19:24 +0330

    Added generateKey and generateShortCode in utils.ts
    Created consts.ts

---
+++ src/helpers/consts.ts
@@ -0,0 +1,4 @@
+export const KEY_SIZE = 32;
+export const SHORT_CODE_SIZE = 6;
+export const KEY_CHARSET_SIZE = 16;
+export const SHORT_CODE_CHARSET_SIZE = 62;
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,5 +1,39 @@
+import {
+  KEY_CHARSET_SIZE,
+  KEY_SIZE,
+  SHORT_CODE_CHARSET_SIZE,
+  SHORT_CODE_SIZE,
+} from "./consts";
 import type { AppSection } from "./types";
+
+const chars62 =
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const chars16 = "0123456789abcdef";
 
 export function customLog(section: AppSection, text: string): void {
   console.log(`[${section.toUpperCase()}] ${text}`);
+}
+
+export function generateShortCode(): string {
+  let result = "";
+
+  for (let i = 0; i < SHORT_CODE_SIZE; i++) {
+    const randomIndex = Math.floor(
+      Math.random() * (SHORT_CODE_CHARSET_SIZE - 1)
+    );
+    result = `${result}${chars62[randomIndex]}`;
+  }
+
+  return result;
+}
+
+export function generateKey(): string {
+  let result = "q";
+
+  for (let i = 0; i < KEY_SIZE - 1; i++) {
+    const randomIndex = Math.floor(Math.random() * (KEY_CHARSET_SIZE - 1));
+    result = `${result}${chars16[randomIndex]}`;
+  }
+
+  return result;
 }
