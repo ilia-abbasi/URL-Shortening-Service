a79e6094b96c1528ad901d3cd9b5a85b38966e64
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-10-04 09:20:15 +0330

    Added some more tests to shorten.spec.ts

--- src/__tests__/e2e/shorten.spec.ts
+++ src/__tests__/e2e/shorten.spec.ts
@@ -6,6 +6,9 @@ import request from "supertest";
 import { createApp } from "../../app/app.js";
 import { clearTable, connectDb, disconnectDb } from "../../database/db.js";
 
+let shortCode: string;
+let key: string;
+
 const app: Application = createApp();
 connectDb(true);
 
@@ -20,5 +23,26 @@ describe("Creating a short URL", () => {
       .post("/shorten")
       .send({ url: "https://goo..gle.com" })
       .expect(400);
+
+    await request(app)
+      .post("/shorten")
+      .send({ url: "htttps://google.com" })
+      .expect(400);
+
+    await request(app)
+      .post("/shorten")
+      .send({ Url: "https://google.com" })
+      .expect(400);
+  });
+
+  it("should successfully create a short url", async () => {
+    const url = "https://google.com";
+    const response = await request(app).post("/shorten").send({ url });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toMatchObject({ data: { url } });
+
+    shortCode = response.body.shortCode;
+    key = response.body.key;
   });
 });
